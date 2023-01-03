c39eeb39e531c5efabf1a40e1088624f6a2b58e5
Face <69168154+face-hh@users.noreply.github.com>
2023-01-03 19:06:39 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('PLEASE RUN THE FOLLOWING: npm i discordmium | REMOVE THIS LINE ONCE DONE');
+
+const Browser = require('discordmium');
+
+Browser("discordBotToken", "guildID") // optional param: time in MS for the next restart
